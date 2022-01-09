af6ca9f447674454b2b724e317e0ca5cb34773d2
Max Wittal <max.wittal@mwittal.de>
2022-01-09 23:39:34 +0100

    dynamic write fix for arrays of optional

--- include/vnx/Type.h
+++ include/vnx/Type.h
@@ -808,7 +808,7 @@ inline void create_dynamic_code(std::vector<uint16_t>& code, std::shared_ptr<con
 /// \private
 template<typename T>
 void create_dynamic_code(std::vector<uint16_t>& code, const vnx::optional<T>& value, bool special) {
-	if(value) {
+	if(!special || value) {
 		vnx::type<T>().create_dynamic_code(code);
 	} else {
 		code.push_back(CODE_NULL);
