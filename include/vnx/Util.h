af03a0e0113a79600ce186d6b7bae5df9933ec2b
Max Wittal <max.wittal@mwittal.de>
2022-04-30 12:57:52 +0200

    generic to_hex_string()

--- include/vnx/Util.h
+++ include/vnx/Util.h
@@ -84,18 +84,22 @@ std::vector<std::string> string_split(const std::string& str, char sep, bool cle
 /// Returns a very good 64-bit random number. (Initialized with nano-second timestamp)
 uint64_t rand64();
 
+/// Converts a binary string to a hex string efficiently. (Without leading "0x")
+std::string to_hex_string(const void* data, const size_t length) {
+	static const char map[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+	std::string str;
+	str.resize(length * 2);
+	for(size_t i = 0; i < length; ++i) {
+		str[(length - i - 1) * 2] = map[((const uint8_t*)data)[i] >> 4];
+		str[(length - i - 1) * 2 + 1] = map[((const uint8_t*)data)[i] & 0x0F];
+	}
+	return str;
+}
+
 /// Converts an integer to a hex string efficiently. (Without leading "0x")
 template<typename T>
 std::string to_hex_string(const T& value) {
-	static const char map[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
-	std::string str;
-	str.resize(sizeof(T) * 2);
-	auto p_value = (const uint8_t*)(&value);
-	for(size_t i = 0; i < sizeof(T); ++i) {
-		str[(sizeof(T) - i - 1) * 2] = map[(p_value[i] & 0xF0) >> 4];
-		str[(sizeof(T) - i - 1) * 2 + 1] = map[p_value[i] & 0x0F];
-	}
-	return str;
+	return to_hex_string((const uint8_t*)&value, sizeof(value));
 }
 
 /// Converts ASCII string to lower case
