9e72a8b4ad063a8008a2787e5991b491309b191e
Max Wittal <max.wittal@mwittal.de>
2024-04-07 17:02:18 +0700

    using RtlGenRandom() on windows

--- src/win32/Util.cpp
+++ src/win32/Util.cpp
@@ -7,34 +7,40 @@
 
 #include <vnx/Util.h>
 
-#include <windows.h>
-#include <Wincrypt.h>
-
-#include <mutex>
 #include <stdexcept>
+#include <windows.h>
+
+
+/* `RtlGenRandom` is used over `CryptGenRandom` on Microsoft Windows based systems:
+ *  - `CryptGenRandom` requires pulling in `CryptoAPI` which causes unnecessary
+ *     memory overhead if this API is not being used for other purposes
+ *  - `RtlGenRandom` is thus called directly instead. A detailed explanation
+ *     can be found here: https://blogs.msdn.microsoft.com/michael_howard/2005/01/14/cryptographically-secure-random-number-on-windows-without-using-cryptoapi/
+ *
+ * In spite of the disclaimer on the `RtlGenRandom` documentation page that was
+ * written back in the Windows XP days, this function is here to stay. The CRT
+ * function `rand_s()` directly depends on it, so touching it would break many
+ * applications released since Windows XP.
+ *
+ * Also note that Rust, Firefox and BoringSSL (thus, Google Chrome and everything
+ * based on Chromium) also depend on it, and that libsodium allows the RNG to be
+ * replaced without patching nor recompiling the library.
+ */
+#define RtlGenRandom SystemFunction036
+#if defined(__cplusplus)
+extern "C"
+#endif
+BOOLEAN NTAPI RtlGenRandom(PVOID RandomBuffer, ULONG RandomBufferLength);
+#pragma comment(lib, "advapi32.lib")
+#endif
 
 
 namespace vnx {
 
 void secure_random_bytes(void* data, const size_t length)
 {
-	static std::mutex mutex;
-	static bool have_init = false;
-	static HCRYPTPROV hCryptProv;
-
-	std::lock_guard<std::mutex> lock(mutex);
-
-	if(!have_init) {
-		if(!CryptAcquireContext(&hCryptProv, NULL,
-				(LPCWSTR)L"Microsoft Base Cryptographic Provider v1.0",
-				PROV_RSA_FULL, CRYPT_VERIFYCONTEXT)) {
-			throw std::runtime_error("CryptAcquireContext() failed");
-		}
-		have_init = true;
-	}
-
-	if(!CryptGenRandom(hCryptProv, length, (BYTE*)data)) {
-		throw std::runtime_error("CryptGenRandom() failed");
+	if(!RtlGenRandom((PVOID)data, (ULONG)length))) {
+		throw std::runtime_error("RtlGenRandom() failed");
 	}
 }
 
