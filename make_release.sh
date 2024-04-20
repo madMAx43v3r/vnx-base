8ec054b6ee26fb8cc0b9d59094f6299dc50bdcb4
Max Wittal <max.wittal@mwittal.de>
2024-04-20 20:48:32 +0700

    use CMAKE_BUILD_TYPE

--- make_release.sh
+++ make_release.sh
@@ -7,9 +7,9 @@ mkdir -p build
 cd build
 
 if [ `uname -o` == "Msys" ]; then
-	cmake -G "MSYS Makefiles" -D VNX_BUILD_TOOLS=ON -D CMAKE_CXX_FLAGS="-O3 -fmax-errors=1" ..
+	cmake -G "MSYS Makefiles" -D VNX_BUILD_TOOLS=ON -D CMAKE_BUILD_TYPE=Release ..
 else
-	cmake -D VNX_BUILD_TOOLS=ON -D CMAKE_CXX_FLAGS="-O3 -fmax-errors=1" ..
+	cmake -D VNX_BUILD_TOOLS=ON ..
 fi
 
 make -j$MAKE_NCPU $@
