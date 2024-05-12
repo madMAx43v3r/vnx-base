26d408d0ff4b21fad281392e1940dd81869b08ba
Max Wittal <max.wittal@mwittal.de>
2024-05-12 14:06:12 +0700

    install.sh CMAKE_BUILD_TYPE="Release"

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@ mkdir -p build
 cd build
 
 if [ `uname -o` == "Msys" ]; then
-	cmake -G "MSYS Makefiles" -D VNX_BUILD_TOOLS=ON -D CMAKE_CXX_FLAGS="-O3" $@ ..
+	cmake -G "MSYS Makefiles" -D VNX_BUILD_TOOLS=ON -D CMAKE_BUILD_TYPE="Release" $@ ..
 else
-	cmake -D VNX_BUILD_TOOLS=ON -D CMAKE_CXX_FLAGS="-O3" $@ ..
+	cmake -D VNX_BUILD_TOOLS=ON -D CMAKE_BUILD_TYPE="Release" $@ ..
 fi
 
 make -j$MAKE_NCPU install
