6e0b5705e98017285f77076f1d9ae131bbf545bd
Max Wittal <max.wittal@mwittal.de>
2025-07-05 20:17:31 +0700

    enum accept() refactor

--- src/Value.cpp
+++ src/Value.cpp
@@ -409,11 +409,7 @@ void accept(Visitor& visitor, TypeInput& in, const TypeCode* type_code, const ui
 	}
 	if(type_code->is_enum) {
 		uint32_t value = 0;
-		if(type_code->field_map.size() > 0) {
-			if(const auto* const _field = type_code->field_map[0]) {
-				vnx::read_value(buf.data() + _field->offset, value, _field->code.data());
-			}
-		}else if(!type_code->is_matched && type_code->fields.size() > 0){
+		if(type_code->fields.size() > 0) {
 			const auto& _field = type_code->fields[0];
 			vnx::read_value(buf.data() + _field.offset, value, _field.code.data());
 		}
